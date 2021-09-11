03a853933c6ad012a108f0b8b455b19d3de27e61
Kimberly De La Paz De León <kimberlydelapazdeleon@gmail.com>
2021-09-11 00:01:11 -0400

    Actualización Rama Kim

--- RegistroAlumno/Form1.Designer.cs
+++ RegistroAlumno/Form1.Designer.cs
@@ -151,10 +151,10 @@ namespace RegistroAlumno
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label5.Location = new System.Drawing.Point(251, 46);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(323, 32);
+            this.label5.Size = new System.Drawing.Size(278, 29);
             this.label5.TabIndex = 2;
             this.label5.Text = "REGISTRO ALUMNOS";
             // 
